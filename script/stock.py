903d6809440c1f69da785f4e01c26aa683f44d22
gh-lliu <lliu@guardanthealth.com>
2019-09-25 11:04:08 -0700

    Added Python notebook

--- script/stock.py
+++ script/stock.py
@@ -1,9 +1,11 @@
-from alpha_vantage import TimeSeries
-import matplotlib
+from alpha_vantage.timeseries import TimeSeries
+import matplotlib.pyplot as plt
+from pprint import pprint
+import pandas
 
 
 ts = TimeSeries(key='YOUR_API_KEY', output_format='pandas')
-data, meta_data = ts.get_intraday(symbol='MSFT',interval='1min', outputsize='full')
-data['close'].plot()
-plt.title('Intraday Times Series for the MSFT stock (1 min)')
+data, meta_data = ts.get_monthly(symbol='AVGO')
+data['4. close'].plot()
+plt.title('Intraday Times Series for the AVGO stock (1 min)')
 plt.show()
